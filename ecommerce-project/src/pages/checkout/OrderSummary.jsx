afd8f28b524252b683a5ec79cf00579e42716775
maciejmrozgg <maciej.mroz.m@gmail.com>
2025-10-16 19:26:10 +0200

    Separate OrderSummary into smaller components

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,26 +1,19 @@
-import dayjs from "dayjs";
 import { CartItemDetails } from "./CartItemDetails";
 import { DeliveryOptions } from "./DeliveryOptions";
+import { DeliveryDate } from "./DeliveryDate";
 
 export function OrderSummary({ cart, deliveryOptions }) {
     return (
         <div className="order-summary">
             {deliveryOptions.length > 0 && cart.map((cartItem) => {
-                const selectedDeliveryOption = deliveryOptions
-                    .find((deliveryOption) => {
-                        return deliveryOption.id === cartItem.deliveryOptionId;
-                    });
 
                 return (
                     <div key={cartItem.product.id} className="cart-item-container">
-                        <div className="delivery-date">
-                            Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-                        </div>
-
+                        <DeliveryDate cartItem={cartItem} deliveryOptions={deliveryOptions} />
+                        
                         <div className="cart-item-details-grid">
-                            
-
-                           <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions}/>
+                            <CartItemDetails cartItem={cartItem} />
+                            <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} />
                         </div>
                     </div>
                 );
