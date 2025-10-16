ce49fe29daf294adc15c963e61d32bdb6c744b24
maciejmrozgg <maciej.mroz.m@gmail.com>
2025-10-16 20:16:43 +0200

    Add dynamic route params and useParams for TrackingPage

--- ecommerce-project/src/pages/tracking/TrackingPage.jsx
+++ ecommerce-project/src/pages/tracking/TrackingPage.jsx
@@ -1,8 +1,11 @@
+import { useParams } from 'react-router';
 import { Link } from 'react-router';
 import { Header } from '../../components/Header';
 import './Tracking.css';
 
 export function TrackingPage({ cart }) {
+    const { orderId, productId } = useParams();
+
     return (
         <>
             <title>Tracking</title>
