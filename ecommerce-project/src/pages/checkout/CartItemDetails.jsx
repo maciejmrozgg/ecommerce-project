d7ed56e193b3592a5827925f05984ef04c95c115
maciejmrozgg <maciej.mroz.m@gmail.com>
2025-11-12 12:55:36 +0100

    Create delete cart item feature

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,6 +1,6 @@
 import { formatMoney } from "../../utilis/money";
 
-export function CartItemDetails({ cartItem }) {
+export function CartItemDetails({ cartItem, deleteCartItem  }) {
     return (
         <>
             <img className="product-image"
@@ -22,7 +22,8 @@ export function CartItemDetails({ cartItem }) {
                     <span className="update-quantity-link link-primary">
                         Update
                     </span>
-                    <span className="delete-quantity-link link-primary">
+                    <span className="delete-quantity-link link-primary"
+                    onClick={deleteCartItem}>
                         Delete
                     </span>
                 </div>
