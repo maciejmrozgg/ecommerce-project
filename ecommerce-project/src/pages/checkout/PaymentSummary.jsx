d5229deba38e4574a2ec680af3b756c64c612361
maciejmrozgg <maciej.mroz.m@gmail.com>
2025-11-12 13:29:30 +0100

    Make create order button work

--- ecommerce-project/src/pages/checkout/PaymentSummary.jsx
+++ ecommerce-project/src/pages/checkout/PaymentSummary.jsx
@@ -1,6 +1,16 @@
+import axios from "axios";
 import { formatMoney } from "../../utilis/money";
+import { useNavigate } from "react-router";
 
-export function PaymentSummary({ paymentSummary }) {
+export function PaymentSummary({ paymentSummary, loadCart }) {
+    const navigate = useNavigate();
+    
+    const createOrder = async () => {
+        await axios.post('/api/orders');
+        await loadCart();
+        navigate('/orders')
+    };
+
     return (
         <div className="payment-summary">
             <div className="payment-summary-title">
@@ -43,7 +53,8 @@ export function PaymentSummary({ paymentSummary }) {
                         </div>
                     </div>
 
-                    <button className="place-order-button button-primary">
+                    <button className="place-order-button button-primary"
+                    onClick={createOrder}>
                         Place your order
                     </button>
                 </>
